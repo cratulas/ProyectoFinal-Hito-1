adc88d5ff58ceb4fdffd05102aa983d4f6316130
Mauricio Mora <mauricios.mora@gmail.com>
2021-11-28 15:02:00 -0300

    Se agregan atributtos Active Storage en modelos Veterinary y User y se modifican vistas

--- app/models/veterinary.rb
+++ app/models/veterinary.rb
@@ -3,4 +3,6 @@ class Veterinary < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :photoVet
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :veterinaries
-  devise_for :users
-  resources :veterinaries
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations'
+  }
+  devise_for :veterinaries, controllers: {
+    sessions: 'veterinaries/sessions',
+    passwords: 'veterinaries/passwords',
+    registrations: 'veterinaries/registrations'
+  }
   root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
