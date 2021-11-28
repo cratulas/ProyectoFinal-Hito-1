6ccd8377b669b878a773881f27c6a31d5c4ffaf5
Mauricio Mora <mauricios.mora@gmail.com>
2021-11-28 03:34:23 -0300

    Se agrega rails admin

--- app/controllers/veterinaries_controller.rb
+++ app/controllers/veterinaries_controller.rb
@@ -1,4 +1,8 @@
 class VeterinariesController < ApplicationController
+  # protect_from_forgery with: :exception
+  # skip_before_action :authenticate_user!, :only => [:index]
+  before_action :authenticate_veterinary!
+
   def index
   end
 end
